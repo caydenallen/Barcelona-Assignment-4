7989f17ad0bb3e0f76ab4cc71e2489e8b95b7cc8
Trevor Orgill <trevororgill@weber.edu>
2017-03-28 12:41:32 -0600

    Added the beggining of each file

---
+++ Barcelona_task1_hw7.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import sys
+
+#		CS3030
+#		File: Barcelona_task1_hw7.py
+#		Author: Team Barcelona
+#		Description:
+#
+#
+
+
+
+def defaultFunction():
+    """
+    Help Comment
+    """
+
+#Main Function
+def main():
+    """
+    Main Function
+    """
+
+if __name__ == "__main__":
+    main()
+
