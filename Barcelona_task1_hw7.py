039584753c869547dc151276c416c861ad36bf55
Cayden Allen <caydenallen@mail.weber.edu>
2017-04-01 17:55:29 -0600

    almost done

--- Barcelona_task1_hw7.py
+++ Barcelona_task1_hw7.py
@@ -12,7 +12,7 @@ import csv
 
 
 
-def test(temp):
+def test(i):
     """"
     test
     """
@@ -20,39 +20,44 @@ def test(temp):
     rOpen = 0
     lOpen = 0
     gsPark = 0
-    pattern = re.compile("/[PpNnDd123rR]/g")
+    pattern = [' P', ' N', ' D', ' 1', ' 2', ' 3', ' R']
     #determin gear shift
-    
-    #LD = (str(temp[1])
-    #RD = temp[2]
-    #CL = (temp[3])
-    #ML = (str(temp[4])
-    #LI = (str(temp[5])
-    #LO = (str(temp[6])
-    #RI = (str(temp[7])
-    #RO = (str(temp[8])
-    GS = (str(temp[9])
-   
-  #  temp[9] == "p"
+    #print(i) 
+    LD = (str(i[1]))
+    RD = (str(i[2]))
+    CL = (str(i[3]))
+    ML = (str(i[4]))
+    LI = (str(i[5]))
+    LO = (str(i[6]))
+    RI = (str(i[7]))
+    RO = (str(i[8]))
+    GS = (str(i[9]))
+  
+    #print(ML, type(ML))
+    #print(RI, type(RI))
 
-    if GS == "p" | GS == "P":
+
+
+  #  temp[9] == "p"
+    
+
+    if GS == " P":
         gsPark = 1
         canOpen = 1
-    elif pattern.match(GS) and len(GS) == 1:
+    elif GS.upper() in pattern:
         gsPark = 0
         canOpen = 0
     else:
         gsPark = 2
         canOpen = 0
-
-    if canOpen == 1 & ML == 1:
+    if canOpen == 1 and int(ML) == 1:
         # test if left door can be opened
-        if (LI == 1 & CL == 0) | LD == 1 | LD == 1:
+        if (int(LI) == 1 and int(CL) == 0) or int(LO) == 1 or int(LD) == 1:
             lOpen = 1
         else:
             lOpen = 0
         # test if right door can be opened
-        if (RI == 1 & CL == 0) | RD == 1 | RD == 1:
+        if (int(RI) == 1 and int(CL) == 0) or int(RO) == 1 or int(RD) == 1:
             rOpen = 1
         else:
             rOpen = 0
@@ -63,11 +68,11 @@ def test(temp):
 
 
     doorText = ""
-    if canOpen == 0 | (lOpen == 0 & rOpen == 0):
+    if canOpen == 0 or (lOpen == 0 and rOpen == 0):
         if gsPark == 2:
             doorText = "Invalid Record: "
         doorText = doorText + "Both doors stay closed."
-    elif lOpen == 1 &rOpen ==1:
+    elif lOpen == 1 and rOpen ==1:
         doorText = "Both doors open."
     else:
         if lOpen == 1:
@@ -86,36 +91,37 @@ def defaultFunction():
     """
     count = 0
     with urlopen("http://icarus.cs.weber.edu/~hvalle/cs3030/data/minivanTest.csv") as data:
+        temp = []
+
         for lines in data:
-            temp = []
-            templines = lines.split()
-            for logs in templines:
-                    temp.append(logs.decode("utf-8"))
-            templines = lines.split()
+            lines = lines[:-1]
+                    #logs = logs[:-1]
+            temp.append(lines.decode("utf-8").split(','))
+        del temp[0]            
             #print(temp)
             
-            
+            #print(templines)
 
         #csv_data = csv.reader(data)
-            #for row in temp:
+            #for row in temp:i
            # print(temp)
-           
-            if temp[0] != "H1,":
-                count = count + 1
-                print("Reading Record", count)
-                print("Left dashboard switch (0 or 1): ", temp[1].strip(','))
-                print("Right dashboard switch (0 or 1): ", temp[2].strip(','))
-                print("Child lock switch (0 or 1): ", temp[3].strip(','))
-                print("Master unlock switch (0 or 1): ", temp[4].strip(','))
-                print("Left inside handle (0 or 1): ", temp[5].strip(','))
-                print("Left outside handle (0 or 1): ", temp[6].strip(','))
-                print("Right inside handle (0 or 1): ", temp[7].strip(','))
-                print("Right outside handle (0 or 1): ", temp[8].strip(','))
-                print("Gear shift position (P,N,D,1,2,3, or R)", temp[9].strip(','))
-       
-       
-    print(test(temp))
-            
+            #print(temp) 
+
+            #if temp[0] != "H1,":
+        for i in temp:            
+            count = count + 1
+            print("Reading Record", count)
+            print("Left dashboard switch (0 or 1): ", i[1])
+            print("Right dashboard switch (0 or 1): ", i[2])
+            print("Child lock switch (0 or 1): ", i[3])
+            print("Master unlock switch (0 or 1): ", i[4])
+            print("Left inside handle (0 or 1): ", i[5])
+            print("Left outside handle (0 or 1): ", i[6])
+            print("Right inside handle (0 or 1): ", i[7])
+            print("Right outside handle (0 or 1): ", i[8])
+            print("Gear shift position (P,N,D,1,2,3, or R)", i[9])
+            print(test(i))
+            print("") 
 
 #Main Function
 def main():
